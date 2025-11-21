aceb6b5d88438e6368e2a064b31a0c23b89dd505
Maximiliano Guaymas <maxikasta480@gmail.com>
2025-11-21 18:03:08 -0300

    marcar todas las notificaciones endpoint

--- notificaciones/urls.py
+++ notificaciones/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import NotificacionesUsuarioView
+from .views import NotificacionesUsuarioView, MarcarTodasLeidasView
 
 urlpatterns = [
     path('mis-notificaciones/', NotificacionesUsuarioView.as_view(), name='mis-notificaciones'),
+    path('notificaciones/marcar-todas-leidas/', MarcarTodasLeidasView.as_view(), name='marcar-todas-leidas'),
 ]
